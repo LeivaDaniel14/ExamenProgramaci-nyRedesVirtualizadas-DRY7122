193406bf9655a26498cf2a438de8891642e5897b
NetAcad DEVASC <changeme@example.com>
2023-07-11 22:57:01 +0000

    ExamenProgramaciónyREdes

---
+++ integrantes.py
@@ -0,0 +1,4 @@
+
+integrante1 = input("Ingresa nombre y apellido del primer integrante:")
+integrante2 = input("Ingresa nombre y apellido del segundo integrante:")
+print("Hola", integrante1 + " y", integrante2 + " ¡Bienvenido al Examen de Programación y Redes Virtualizas!")
